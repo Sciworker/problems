d94ceb8ab810d36cddf24c5666fd530a5a4aa181
Peter F <ivanov198405@gmail.com>
2022-09-25 17:05:49 +0300

    Add first problem on tkinter

--- problembook.py
+++ problembook.py
@@ -1,4 +1,4 @@
-"""import tkinter
+import tkinter
 def UpdateLists():
     global taskButtonsList
     for i in range(len(taskButtonsList)):
@@ -28,35 +28,5 @@ addButton = tkinter.Button(window, width= 70, height=2, text= "добавить 
 addButton.place(x=0, y= 70)
 listLabel = tkinter.Label(window, text="список задач")
 listLabel.place(x=0, y=115)
-window.mainloop()"""
-
-"""ЗАДАЧА №579		
-Модуль суммы
-Дана последовательность целых чисел. 
-Требуется найти подпоследовательность заданной последовательности с максимальным модулем суммы 
-входящих в нее чисел. Напомним, что модуль целого числа x равняется x, если x ≥ 0 и -x, если x < 0."""
-
-posled = int(input())
-array = []
-for i  in range(0, posled):
-    array.append(int(input()))
-sum_pol = 0
-sum_otr = 0
-mod=0
-for i in range(0, len(array),1):
-    if array[i] > 0:
-        sum_pol = sum_pol+array[i] 
-    if array[i] < 0:
-        sum_otr = sum_otr+array[i] 
-if abs(sum_otr) > abs(sum_pol):
-    mod = (-1)
-else:
-    mod = 1
-for i in range(0, len(array),1):
-    if mod == 1:
-        if array[i]>0:
-            print(i)
-    else:
-        if array[i]<0:
-            print(i)
+window.mainloop()
 
